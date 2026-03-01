9f45f1c193559ce234d94818fcb546bbef9697ff
MAHAMED-AL7AKM <mhmdalhakmlfkalarqam@gmail.com>
2026-03-01 02:14:54 +0200

    config.py

--- config.py
+++ config.py
@@ -1,21 +1,24 @@
 import os
+import base64
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Gemini API
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+# Gemini API Key (مشفر)
+ENCODED_KEY = "QUl6YVN5Qi1ubnpiYk00MU1LZXpQa1ZVU1M1VVhLbGNBQnVtbUxn"
+GEMINI_API_KEY = base64.b64decode(ENCODED_KEY).decode()
 
-# المسار المباشر لمجلد Download (بعد termux-setup-storage)
-BASE_DIR = os.path.expanduser("~/storage/download/7AKM OSINT")
+# المسار الصحيح للذاكرة الخارجية عبر Termux
+BASE_DIR = os.path.expanduser("~/storage/downloads")
+SHARED_DIR = os.path.join(BASE_DIR, "7AKM OSINT")
 
 # المجلدات الفرعية
-REPORTS_DIR = os.path.join(BASE_DIR, "reports")
-IDENTITIES_DIR = os.path.join(BASE_DIR, "identities")
+REPORTS_DIR = os.path.join(SHARED_DIR, "reports")
+IDENTITIES_DIR = os.path.join(SHARED_DIR, "identities")
 
 # إنشاء جميع المجلدات
-os.makedirs(BASE_DIR, exist_ok=True)
+os.makedirs(SHARED_DIR, exist_ok=True)
 os.makedirs(REPORTS_DIR, exist_ok=True)
 os.makedirs(IDENTITIES_DIR, exist_ok=True)
 
-print(f"[✓] Files will be saved in: {BASE_DIR}")
+print(f"[✓] Files will be saved in: {SHARED_DIR}")
